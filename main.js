8c4a8ca4843946595e5edadc45e8ff6326a8e2a7
fifffa <147136171+fifffa@users.noreply.github.com>
2025-10-27 22:33:01 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -459,13 +459,13 @@ async function fetchByCategory({ categoryId }) {
 
   const productCategories = await ProductCategories.find();
   const total = productCategories.length;
-  const baseSize = Math.floor(total / 13); // 기본 크기
-  let remainder = total % 13; // 남는 개수
+  const baseSize = Math.floor(total / 14); // 기본 크기
+  let remainder = total % 14; // 남는 개수
 
   const divided = [];
   let start = 0;
 
-  for (let i = 0; i < 13; i++) {
+  for (let i = 0; i < 14; i++) {
     // 나머지가 남아있으면 이 그룹은 +1개 더 받음
     const extra = remainder > 0 ? 1 : 0;
     const end = start + baseSize + extra;
